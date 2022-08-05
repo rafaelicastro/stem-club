a76d4e2d98d0c7ba3da35842681865f93824c87e
Rafael Castro <rafaelignaciocastro@gmail.com>
2022-08-05 17:11:57 -0400

    starting to make other pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './Pages/Home.js';
+import Physics from './Pages/Physics';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/physics">
+        <Route path="srel" element={<Physics.SRel />} />
+      </Route>
     </Routes>
   );
 }
