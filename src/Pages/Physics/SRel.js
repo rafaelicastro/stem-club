db9c8c55efacda2d0028aee8c4286762e21e617f
Rafael Castro <rafaelignaciocastro@gmail.com>
2022-08-06 17:38:20 -0400

    getting ready to host on gh pages

--- src/Pages/Physics/SRel.js
+++ src/Pages/Physics/SRel.js
@@ -1,4 +1,4 @@
-import { Heading, VStack, Stack, Text } from '@chakra-ui/react';
+import { Heading, VStack, Text } from '@chakra-ui/react';
 import { NavBar } from '../../Components/NavBar';
 import { Slide } from '../../Components/Slide';
 
@@ -7,15 +7,14 @@ export function SRel() {
 		<>
 			<NavBar />
 
-            <Stack textAlign={'center'} align={'center'}>
-                <Heading
-                    fontWeight={600}
-                    fontSize={{ base: '4xl', sm: '5xl', md: '1xl' }}
-                    lineHeight={'110%'}
-                    >
-                        <Text as={'span'} color={'blue.400'}>Special Relativity</Text>
-                </Heading>
-            </Stack>
+            <Heading
+                textAlign={'center'} align={'center'}
+                fontWeight={600}
+                fontSize={{ base: '4xl', sm: '5xl', md: '1xl' }}
+                lineHeight={'110%'}
+            >
+                    <Text as={'span'} color={'blue.400'}>Special Relativity</Text>
+            </Heading>
 
             <VStack paddingY={100} spacing={150} align={"center"}>
                 <Slide number={1} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." link="https://docs.google.com/presentation/d/1-IyBCMHnuoH5M5hNzZ37sQEvxT7PM4VyMORxeNNMQHo/edit#slide=id.p"/>
