dd8f0debb10e0d7f99fd229829f56c6d3d083add
Rafael Castro <rafaelignaciocastro@gmail.com>
2022-08-13 15:51:27 -0400

    deleted unnessecary code

--- src/Pages/Physics/SRel.js
+++ src/Pages/Physics/SRel.js
@@ -1,28 +1,3 @@
-// import { Heading, VStack, Text } from '@chakra-ui/react';
-// import { NavBar } from '../../Components/NavBar';
-// import { Slide } from '../../Components/Slide';
-
-// export function SRel() {
-// 	return (
-// 		<>
-// 			<NavBar />
-
-//             <Heading
-//                 textAlign={'center'} align={'center'}
-//                 fontWeight={600}
-//                 fontSize={{ base: '4xl', sm: '5xl', md: '1xl' }}
-//                 lineHeight={'110%'}
-//             >
-//                     <Text as={'span'} color={'blue.400'}>Special Relativity</Text>
-//             </Heading>
-
-//             <VStack paddingY={100} spacing={150} align={"center"}>
-//                 <Slide title={1} desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." link="https://docs.google.com/presentation/d/1-IyBCMHnuoH5M5hNzZ37sQEvxT7PM4VyMORxeNNMQHo/edit#slide=id.p"/>
-//             </VStack>
-// 		</>
-// 	);
-// }
-
 import { Container, Heading, Stack, Text, VStack } from '@chakra-ui/react';
 import { NavBar } from '../../Components/NavBar';
 import { Slide } from '../../Components/Slide';
